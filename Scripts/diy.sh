169191763003e4bad3ca233d925d6d55e889e7d4
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-09-07 06:01:58 +0800

    测试

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,9 +4,12 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
-#rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-minieap
+rm -rf feeds/packages/net/{minieap}
 #cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
-
+#rm -rf feeds/luci/applications/{*-adguardhome,*-bypass,*-homeproxy,*-passwall*,*-ssr-plus,*-mosdns,*-smartdns,*-serverchan,*-ikoolproxy,*-qbittorrent}
+#rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray-core,v2raya,sing*,smartdns,openclash,homeproxy,serverchan,ikoolproxy,qbittorrent,pdnsd-alt}
+#cp -rf feeds/smpackage/{alist,adguardhome,mosdns,xray*,v2ray-core,v2raya,sing*,smartdns,openclash,homeproxy,serverchan,ikoolproxy,qbittorrent,pdnsd-alt} feeds/packages/net/
 
 rm -rf feeds/smpackage/applications/*homeproxy
 ./scripts/feeds install wrtbwmon
