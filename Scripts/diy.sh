570ada0e628a23c210b8caa5a527de17ebd60717
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-09-06 22:01:13 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,10 +4,10 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
-rm -rf feeds/luci/applications/luci-app-openclash
-cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
+#rm -rf feeds/luci/applications/luci-app-openclash
+#cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
 
-rm -rf feeds/smpackage/applications/homeproxy
+
 rm -rf feeds/smpackage/applications/*homeproxy
 ./scripts/feeds install wrtbwmon
 ./scripts/feeds install -a  
