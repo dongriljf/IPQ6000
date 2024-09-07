93d64ef3d219a8b5b12b1dac6b066aa32f6789fb
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-09-07 21:47:28 +0800

    hkgjfk

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -5,7 +5,7 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 rm -rf feeds/luci/applications/luci-app-minieap
-cp -rf feeds/smpackage/luci-app-minieap   feeds/luci/applications/luci-app-minieap
+cp -rf feeds/smpackage/luci-app-minieap   feeds/luci/applications/
 rm -rf feeds/packages/net/{minieap}
 #cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
 #rm -rf feeds/luci/applications/{*-adguardhome,*-bypass,*-homeproxy,*-passwall*,*-ssr-plus,*-mosdns,*-smartdns,*-serverchan,*-ikoolproxy,*-qbittorrent}
